301b2c2c66f11cdb57364aa9c9084fd7d9dcc0d7
John Mark Harrell <hijohnmark@gmail.com>
2023-10-13 12:08:08 +0700

    complete phase 1 control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(someValue){
   // Write your code here!
+if (someValue <= 400) {
+return 'This one is on me!'
+} else if (someValue > 400 && someValue < 2000) {
+  return 'That will be twenty bucks.'
+} else if (someValue > 2000 && someValue < 2500) {
+  return 'I will gladly take your thirty bucks.'
+} else {
+  return 'No can do.'
+}
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(someCity){
   // Write your code here!
+  someCity = someCity === 'NYC' ? 'Ok, sounds good.' : 'No go.';
+  return someCity;
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(someTip){
   // Write your code here!
+  switch (someTip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+      case 'not as generous':
+        return 'Thank you.';
+        break;
+        default: 
+        return 'Bye.';
+        break;
+  }
 }
